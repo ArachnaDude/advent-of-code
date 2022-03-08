d68a30762d0cb8df76b1b2c7145cace848e4508b
Matt <cunning.distraction@googlemail.com>
2022-03-08 22:50:42 +0000

    complete day2 part2, all tests passing

--- day2/day2.js
+++ day2/day2.js
@@ -6,7 +6,7 @@ directionFinder = (arr) => {
     depth: 0,
     multValue: 0,
   };
-  console.log(directionObject, "pre for loop");
+
   for (let i = 0; i < arr.length; i++) {
     let direction = arr[i][0];
     let increment = arr[i][1];
@@ -30,6 +30,39 @@ directionFinder = (arr) => {
   return directionObject;
 };
 
-console.log(directionFinder(formattedDirections));
+//console.log(directionFinder(formattedDirections));
 
-module.exports = directionFinder;
+directionFinderWithAim = (arr) => {
+  const directionObject = {
+    horizontalPosition: 0,
+    depth: 0,
+    aim: 0,
+    multValue: 0,
+  };
+
+  for (let i = 0; i < arr.length; i++) {
+    let direction = arr[i][0];
+    let increment = arr[i][1];
+
+    switch (direction) {
+      case "down":
+        directionObject.aim += increment;
+        break;
+      case "up":
+        directionObject.aim -= increment;
+        break;
+      case "forward":
+        directionObject.horizontalPosition += increment;
+        directionObject.depth += directionObject.aim * increment;
+        break;
+    }
+  }
+  directionObject.multValue =
+    directionObject.horizontalPosition * directionObject.depth;
+
+  return directionObject;
+};
+
+console.log(directionFinderWithAim(formattedDirections));
+
+module.exports = { directionFinder, directionFinderWithAim };
--- day2/day2.test.js
+++ day2/day2.test.js
@@ -1,4 +1,4 @@
-const directionFinder = require("./day2");
+const { directionFinder, directionFinderWithAim } = require("./day2");
 
 describe("directionFinder", () => {
   test("when passed an array of direction arrays, returns an object", () => {
@@ -38,7 +38,7 @@ describe("directionFinder", () => {
     const functionCall = directionFinder(testArr);
     expect(functionCall.depth).toBe(2);
   });
-  test.only("multValue property is final horizontal position and depth multiplied", () => {
+  test("multValue property is final horizontal position and depth multiplied", () => {
     const testArr = [
       ["forward", 2],
       ["down", 3],
@@ -48,5 +48,73 @@ describe("directionFinder", () => {
     ];
     const functionCall = directionFinder(testArr);
     expect(functionCall.multValue).toBe(20);
+  });
+});
+
+describe.only("directionFinderWithAim", () => {
+  test("when passed an array of direction arrays, returns an object", () => {
+    const testArr = [
+      ["forward", 1],
+      ["down", 1],
+    ];
+    const functionCall = directionFinderWithAim(testArr);
+    const type = typeof functionCall;
+    expect(type).toBe("object");
+  });
+  test("returned object has properties of horizontalPosition, depth and aim", () => {
+    const testArr = [
+      ["forward", 1],
+      ["down", 1],
+    ];
+    const functionCall = directionFinderWithAim(testArr);
+    expect(functionCall).toHaveProperty("horizontalPosition");
+    expect(functionCall).toHaveProperty("depth");
+    expect(functionCall).toHaveProperty("aim");
+  });
+  test("'down' increases aim, 'up' decreases aim", () => {
+    const testArr = [
+      ["down", 2],
+      ["down", 2],
+      ["up", 1],
+    ];
+    const functionCall = directionFinderWithAim(testArr);
+    expect(functionCall.aim).toBe(3);
+  });
+  test("'forward' increases horizontalPosition by number specified, and increases depth by aim * number specified", () => {
+    const testArr = [
+      ["down", 2],
+      ["forward", 5],
+    ];
+    const functionCall = directionFinderWithAim(testArr);
+    expect(functionCall.aim).toBe(2);
+    expect(functionCall.horizontalPosition).toBe(5);
+    expect(functionCall.depth).toBe(10);
+  });
+  test("'forward' works with both 'down' and 'up' values, in multiple steps", () => {
+    const testArr = [
+      ["forward", 5],
+      ["down", 5],
+      ["forward", 8],
+      ["up", 3],
+      ["down", 8],
+      ["forward", 2],
+    ];
+    const functionCall = directionFinderWithAim(testArr);
+    expect(functionCall.horizontalPosition).toBe(15);
+    expect(functionCall.depth).toBe(60);
+  });
+  test("returned object has a property of multValue, value of which is horizontalPosition * depth", () => {
+    const testArr = [
+      ["forward", 5],
+      ["down", 5],
+      ["forward", 8],
+      ["up", 3],
+      ["down", 8],
+      ["forward", 2],
+    ];
+    const functionCall = directionFinderWithAim(testArr);
+    expect(functionCall.horizontalPosition).toBe(15);
+    expect(functionCall.depth).toBe(60);
+    expect(functionCall.multValue).toBe(900);
   });
 });
