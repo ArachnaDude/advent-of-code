c39b5814d220d5dde0299fb1174a0ab737076836
Matt <cunning.distraction@googlemail.com>
2022-03-09 03:00:18 +0000

    format diagnostics into usable form

---
+++ day3/day3.js
@@ -0,0 +1,16 @@
+const diagnosticsArr = require("./diagnostics");
+
+const powerConsumption = (arr) => {
+  const powerObj = { gammaRate: 0, epsilonRate: 0 };
+
+  for (let i = 0; i < arr.length; i++) {
+    let entry = arr[i];
+    const slicedStr = entry.slice(0);
+  }
+
+  return powerObj;
+};
+
+module.exports = powerConsumption;
+
+//slice to chop down the string for each iteration?
---
+++ day3/day3.test.js
@@ -0,0 +1,78 @@
+const powerConsumption = require("./day3");
+
+describe("powerConsumption", () => {
+  test("when passed an array, function returns an object", () => {
+    const testArr = [
+      "00100",
+      "11110",
+      "10110",
+      "10111",
+      "10101",
+      "01111",
+      "00111",
+      "11100",
+      "10000",
+      "11001",
+      "00010",
+      "01010",
+    ];
+    const functionCall = powerConsumption(testArr);
+    const type = typeof functionCall;
+    expect(type).toBe("object");
+  });
+  test("returned object has properties 'gamma rate' and 'epsilon rate'", () => {
+    const testArr = [
+      "00100",
+      "11110",
+      "10110",
+      "10111",
+      "10101",
+      "01111",
+      "00111",
+      "11100",
+      "10000",
+      "11001",
+      "00010",
+      "01010",
+    ];
+    const functionCall = powerConsumption(testArr);
+    expect(functionCall).toHaveProperty("gammaRate");
+    expect(functionCall).toHaveProperty("epsilonRate");
+  });
+  test("assigns mode of the first digit to gammaRate", () => {
+    const testArr = [
+      "0",
+      "1",
+      "1",
+      "1",
+      "1",
+      "0",
+      "0",
+      "1",
+      "1",
+      "1",
+      "0",
+      "0",
+    ];
+    const functionCall = powerConsumption(testArr);
+    expect(functionCall.gammaRate).toBe(1);
+  });
+  test.only("assigns mode of first and second digit to gammaRate", () => {
+    const testArr = [
+      "00",
+      "11",
+      "10",
+      "10",
+      "10",
+      "01",
+      "00",
+      "11",
+      "10",
+      "11",
+      "00",
+      "01",
+    ];
+    const functionCall = powerConsumption(testArr);
+    expect(functionCall.gammaRate).toBe(10);
+  });
+});
