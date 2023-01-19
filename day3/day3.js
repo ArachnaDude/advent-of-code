ffc7fcbd8cba5fee1c035302534d030dd8757dc2
Matt <cunning.distraction@googlemail.com>
2023-01-19 03:50:30 +0000

    add function to calculate anti-mode from mode, and convert to decimal from binary

--- day3/day3.js
+++ day3/day3.js
@@ -4,6 +4,7 @@ const powerConsumption = (arr) => {
   const powerObj = { gammaRate: 0, epsilonRate: 0 };
 
   const modeArray = [];
+  const antiModeArray = [];
   const strLength = arr[0].length;
   for (let i = 0; i < strLength; i++) {
     let digitArr = arr.map((element) => element[i]);
@@ -19,11 +20,20 @@ const powerConsumption = (arr) => {
       }
       return zeroCount > oneCount ? 0 : 1;
     };
+    const antiMode = (num) => {
+      return num === 0 ? 1 : 0;
+    };
     console.log(digitArr, `array of digits at position ${i}`);
     console.log(getMode(digitArr), `mode of digits at position ${i}`);
+    console.log(
+      antiMode(getMode(digitArr)),
+      `anti-mode of digits at position ${i}`
+    );
     modeArray.push(getMode(digitArr));
+    antiModeArray.push(antiMode(getMode(digitArr)));
   }
   powerObj.gammaRate = parseInt(modeArray.join(""), 2);
+  powerObj.epsilonRate = parseInt(antiModeArray.join(""), 2);
 
   return powerObj;
 };
