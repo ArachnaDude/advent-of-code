14d10f769dbafd71aeb3f5fdfa956663a9cae054
Matt <cunning.distraction@googlemail.com>
2023-01-19 03:21:41 +0000

    successfully determine mode of elements up to 2 digits long, and convert to decimal

--- day3/day3.js
+++ day3/day3.js
@@ -3,21 +3,31 @@ const diagnosticsArr = require("./diagnostics");
 const powerConsumption = (arr) => {
   const powerObj = { gammaRate: 0, epsilonRate: 0 };
 
-  for (let i = 0; i < arr.length; i++) {
-    let entry = arr[i];
-    console.log(entry, "<< array element");
-    for (let j = 0; j < entry.length; j++) {
-      console.log(entry[j], "<< string in entry");
-    }
+  const modeArray = [];
+  const strLength = arr[0].length;
+  for (let i = 0; i < strLength; i++) {
+    let digitArr = arr.map((element) => element[i]);
+    const getMode = (array) => {
+      let zeroCount = 0;
+      let oneCount = 0;
+      for (let j = 0; j < array.length; j++) {
+        if (array[j] === "0") {
+          zeroCount++;
+        } else {
+          oneCount++;
+        }
+      }
+      return zeroCount > oneCount ? 0 : 1;
+    };
+    console.log(digitArr, `array of digits at position ${i}`);
+    console.log(getMode(digitArr), `mode of digits at position ${i}`);
+    modeArray.push(getMode(digitArr));
   }
+  powerObj.gammaRate = parseInt(modeArray.join(""), 2);
 
   return powerObj;
 };
 
 module.exports = powerConsumption;
 
-// gamma and epsilon should be arrays, then join.
-// we don't need to coerce final resuult to numbers
-// strings will work fine
-
-// to calculate mode
+//
