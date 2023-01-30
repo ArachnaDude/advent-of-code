a483642b1b5115b9669985f00fa48955e356aacc
Matt <cunning.distraction@googlemail.com>
2023-01-30 01:43:41 +0000

    add functionality to remove non-modal values from up to 2 digit array of strings

--- day3/day3.js
+++ day3/day3.js
@@ -39,16 +39,47 @@ const powerConsumption = (arr) => {
   return powerObj;
 };
 
-// powerConsumption(diagnosticsArr);
-
 const lifeSupport = (arr) => {
   const powerObj = {
     "O2 rate": 0,
     "CO2 scrubber rate": 0,
     lifeSupportRating: 0,
+    modeArrayCopy: [...arr],
+    antiModeArrayCopy: [...arr],
   };
+
+  const strLength = arr[0].length;
+
+  for (let i = 0; i < strLength; i++) {
+    console.log(powerObj.modeArrayCopy, "modeArrayCopy before loop");
+    // provides an array of all values at position i
+    let digitArr = powerObj.modeArrayCopy.map((element) => element[i]);
+    console.log(digitArr, "array of digits at position", i);
+    let copiedArr = [...digitArr];
+    const getMode = (array) => {
+      let zeroCount = 0;
+      let oneCount = 0;
+      for (let j = 0; j < array.length; j++) {
+        if (array[j] === "0") zeroCount++;
+        else oneCount++;
+      }
+      return zeroCount > oneCount ? "0" : "1";
+    };
+    let modeOfNthDigit = getMode(copiedArr);
+    console.log(modeOfNthDigit, "mode of element", i);
+    const filteredArray = powerObj.modeArrayCopy.filter((digit) => {
+      console.log(digit, "digit");
+      return digit[i] === modeOfNthDigit;
+    });
+    console.log(filteredArray, "filteredArray");
+    powerObj.modeArrayCopy = filteredArray;
+  }
 
   return powerObj;
 };
 
 module.exports = { powerConsumption, lifeSupport };
+
+//loop through first digit of each element.
+//calculate mode of first digit
+// if first digit != mode, delete it
